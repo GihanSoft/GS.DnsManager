fce46a91f456812e1ba1b0730e49717629f37bce
Mohammad Babayi <chief.mb1976@gmail.com>
2024-12-30 14:24:43 +0330

    feat: wire up Blazor

--- src/GS.DnsManager/App.xaml.cs
+++ src/GS.DnsManager/App.xaml.cs
@@ -1,10 +1,48 @@
 ﻿using System.Windows;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace GS.DnsManager;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 internal sealed partial class App : Application
 {
-}
+    public static new App Current => (App)Application.Current;
 
+    private IHost? _host;
+
+    public IServiceProvider ServiceProvider => _host!.Services;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var builder = Host.CreateApplicationBuilder(e.Args);
+        ConfigureServices(builder);
+        var app = builder.Build();
+        _host = app;
+        Resources.Add("services", ServiceProvider);
+        app.StartAsync().GetAwaiter().GetResult();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        _host?.StopAsync().GetAwaiter().GetResult();
+        _host?.Dispose();
+    }
+
+    private static void ConfigureServices(HostApplicationBuilder builder)
+    {
+        builder.Services.AddWpfBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
+#endif
+    }
+}
